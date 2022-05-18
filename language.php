d9111bc20dec4a851677208574dfb351b99c51d9
K.H <mail@address.co.jp>
2022-05-18 21:48:01 +0900

    first commit

---
+++ language.php
@@ -0,0 +1,55 @@
+<?php
+
+//language setting
+$GLOBALS['lang']='en';//other en
+
+selectLang();
+
+function selectLang() {
+    switch($GLOBALS['lang']) {
+    case 'jp':
+        lang_jp();
+        break;
+    case 'en':
+    default:
+        lang_en();
+        break;
+    }
+}
+
+function lang_jp() {
+    $GLOBALS['messages']['link-page-title']='ファイル転送サーバへのリンクページ';
+    $GLOBALS['messages']['link-h1-title']='ファイル転送サーバ';
+    $GLOBALS['messages']['link-ip']='URL:';
+    $GLOBALS['messages']['upload-end']='件アップロードしました';
+    $GLOBALS['messages']['upload-page-title']='ファイル転送サーバ';
+    $GLOBALS['messages']['upload-page-send']='送信';
+    $GLOBALS['messages']['upload-page-select']='ファイル選択';
+    $GLOBALS['messages']['upload-page-no-select']='ファイル選択されていません';
+    $GLOBALS['messages']['upload-page-cancel']='中止';
+    
+    $GLOBALS['messages']['ajax-ok']=' 成功';
+    $GLOBALS['messages']['ajax-ng']=' 失敗';
+    $GLOBALS['messages']['ajax-no-file']=' ファイルがありませんでした';
+
+}
+
+function lang_en() {
+    $GLOBALS['messages']['link-page-title']='simple file transfer entrance';
+    $GLOBALS['messages']['link-h1-title']='scan qr code, and go to link page';
+    $GLOBALS['messages']['link-ip']='URL:';
+    $GLOBALS['messages']['upload-end']=' files uploaded.';
+    $GLOBALS['messages']['upload-page-title']='simple file transfer';
+    $GLOBALS['messages']['upload-page-send']='send';
+    $GLOBALS['messages']['upload-page-select']='select files';
+    $GLOBALS['messages']['upload-page-no-select']='no file selected';
+    $GLOBALS['messages']['upload-page-cancel']='cancel';
+    
+    $GLOBALS['messages']['ajax-ok']=' succeed';
+    $GLOBALS['messages']['ajax-ng']=' failed !';
+    $GLOBALS['messages']['ajax-no-file']='file does not exist';
+
+}
+
+
+?>
---
+++ upload.php
@@ -0,0 +1,112 @@
+<?php
+    require_once('./language.php');
+?>
+
+<!DOCTYPE html>
+    <html lang="<?php echo $GLOBALS['lang']; ?>">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $GLOBALS['messages']['upload-page-title']; ?></title>
+		<style>
+			img {
+				max-width: 300px;
+				max-height: 300px;
+			}
+			input[type="file"] {
+    			display: none;
+			}
+			ul {
+				list-style: none;
+			}
+		</style>
+	</head>
+	<body>
+		<div>
+		<h1><?php echo $GLOBALS['messages']['upload-page-title']; ?></h1>
+		</div>
+		<div>
+		<input id="hiddenfiles" type="file" onchange="changeFiles(event)" multiple/>
+		<button style="width:10em;font-size:100%;" onclick="document.getElementById('hiddenfiles').click();"><?php echo $GLOBALS['messages']['upload-page-select']; ?></button>
+		</div>
+		<ul id="file-list">
+			<li><?php echo $GLOBALS['messages']['upload-page-no-select']; ?>
+		</ul>
+		<div>
+		<button style="width:10em;font-size:100%;" onclick="sendFiles()"><?php echo $GLOBALS['messages']['upload-page-send']; ?></button>
+		</div>
+		<div id="message">
+		</div>
+	<script>
+		let targetFiles=[];
+		let fileList = document.getElementById('file-list');
+		let message = document.getElementById('message');
+
+		function changeFiles(event) {
+			
+			targetFiles=[];
+			
+			while(fileList.firstChild){
+				fileList.removeChild(fileList.firstChild);
+			}
+
+			if (event.target.files.length <= 0) {
+				let li = document.createElement('li');
+				li.innerText='<?php echo $GLOBALS['messages']['upload-page-no-select']; ?>';
+				fileList.appendChild(li);
+			} else {
+				for(let i = 0, max =event.target.files.length; i < max; i++) {
+
+					let li = document.createElement('li');
+					li.innerText=event.target.files[i].name+' ';
+					let button = document.createElement('button');
+					button.innerText='<?php echo $GLOBALS['messages']['upload-page-cancel']; ?>';
+					
+					button.onclick=()=>{
+						targetFiles = targetFiles.filter(v=>v!=event.target.files[i]);
+						li.remove();
+					}
+
+					li.appendChild(button);
+
+					let br = document.createElement('br');
+					li.appendChild(br);
+					let img = document.createElement('img');
+					li.appendChild(img);
+					fileList.appendChild(li);
+
+					targetFiles.push(event.target.files[i]);
+
+					let fileReader = new FileReader();
+					fileReader.onload = function(_) {
+						img.src = fileReader.result;
+					};
+					fileReader.readAsDataURL(event.target.files[i]);
+				}
+
+			}
+		}
+		async function sendFiles() {
+  			let formData = new FormData();
+
+			for (let i = 0; i < targetFiles.length; i++) {
+  				formData.append("uploadfiles[]", targetFiles[i]);
+			}
+
+			await fetch('./ajax-upload.php', { 
+				method: "POST", 
+				body: formData
+			})
+			.then(response=> response.json())
+			.then(data => {
+				if (data.result==true) {
+					message.innerHTML=data.message;
+				} else {
+					message.innerText='<?php echo $GLOBALS['messages']['ajax-ng']; ?>';
+				}	
+			});
+		}
+	</script>
+	</body>
+</html>
